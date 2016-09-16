d1deb3ba064b53d75a2760521a3765c8927482f3
David Dias <daviddias.p@gmail.com>
2016-09-16 00:19:23 -0400

    1st thousand blocks

---
+++ test/vm-1st-1000.spec.js
@@ -0,0 +1,71 @@
+'use strict'
+/* eslint-env mocha */
+
+const ethereum = require('../src')
+const expect = require('chai').expect
+const Block = require('ethereumjs-block')
+const BlockHeader = require('ethereumjs-block/header.js')
+// const parallel = require('run-parallel')
+const thousand = require('./data/first-1000-blocks.json')
+const async = require('async')
+const utils = require('ethereumjs-util')
+const rlp = utils.rlp
+
+describe('process 1st 1000 blocks', () => {
+  let eth
+
+  it('spawn a node', () => {
+    eth = new ethereum.Node()
+  })
+
+  it.skip('load pre data to setup vm', (done) => {
+    // eth.vm.setup(testData, done)
+  })
+
+  it('add genesis block', (done) => {
+    const decoded = rlp.decode(thousand[0])
+    console.log(decoded)
+    /*
+    const genesisBlock = new Block()
+
+    genesisBlock.header = new BlockHeader(
+                            testData.genesisBlockHeader
+                          )
+    eth.vm._blockchain.putGenesis(genesisBlock, done)
+    */
+  })
+
+  /*
+  it.skip('add more blocks', (done) => {
+    async.eachSeries(testData.blocks, eachBlock, done)
+
+    function eachBlock (raw, cb) {
+      let block
+      try {
+        block = new Block(new Buffer(raw.rlp.slice(2), 'hex'))
+
+        // forces the block into thinking they are homestead
+        block.header.isHomestead = () => { return true }
+        block.uncleHeaders.forEach((uncle) => {
+          uncle.isHomestead = () => { return true }
+        })
+
+        eth.vm._blockchain.putBlock(block, cb)
+      } catch (err) { cb(err) }
+    }
+  })
+
+  it.skip('run through the blockchain', (done) => {
+    eth.vm.runBlockchain((err) => {
+      expect(err).to.not.exist
+      eth.vm.blockchain.getHead((err, block) => {
+        expect(err).to.not.exist
+        const currentHead = '0x' + eth.vm._blockchain.meta.rawHead.toString('hex')
+        const expected = testData.lastblockhash
+        expect(currentHead).to.equal(expected)
+        done()
+      })
+    })
+  })
+  */
+})
